53961d5e04860c7505447df811fb1678ad52edc7
Matthew Miner <mminer237@gmail.com>
2020-02-14 18:24:49 -0500

    Switch to pickr

--- js/main.js
+++ js/main.js
@@ -1,2 +1,30 @@
 "use strict"
 
+Array.from(document.querySelectorAll('.color-picker')).forEach(container => {
+	const opacity = !container.classList.contains("opaque");
+	const pickr = Pickr.create({
+		el: container,
+		theme: 'classic',
+		lockOpacity: !opacity,
+		adjustableNumbers: false,
+
+		components: {
+			// Main components
+			preview: true,
+			opacity: opacity,
+			hue: true,
+
+			// Input / output Options
+			interaction: {
+				hex: true,
+				rgba: true,
+				hsla: true,
+				hsva: true,
+				cmyk: true,
+				input: true,
+				clear: true,
+				save: true
+			}
+		}
+	});
+});
