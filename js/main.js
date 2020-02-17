3b3a4ff16296fd82d2d74bbf5df90b022391d27f
Matthew Miner <mminer237@gmail.com>
2020-02-17 02:48:08 -0500

    Add color helper functions

--- js/main.js
+++ js/main.js
@@ -87,8 +87,7 @@ class AlternateColor extends HTMLElement {
 		});
 	}
 	setColorNear(mainColor) {
-		mainColor = mainColor.substring(1);
-		this.color = `#${mainColor}`;
+		this.color = mainColor;
 		this.colorInsideElement.style.backgroundColor = this.color;
 	}
 }
@@ -102,27 +101,61 @@ document.getElementById("refresh-alts").addEventListener("click", e => {
 	refreshAlternates();
 });
 
-function HSVtoRGB(h, s, v) {
-	var r, g, b, i, f, p, q, t;
-	if (arguments.length === 1) {
-		s = h.s, v = h.v, h = h.h;
-	}
-	i = Math.floor(h * 6);
-	f = h * 6 - i;
-	p = v * (1 - s);
-	q = v * (1 - f * s);
-	t = v * (1 - (1 - f) * s);
-	switch (i % 6) {
-		case 0: r = v, g = t, b = p; break;
-		case 1: r = q, g = v, b = p; break;
-		case 2: r = p, g = v, b = t; break;
-		case 3: r = p, g = q, b = v; break;
-		case 4: r = t, g = p, b = v; break;
-		case 5: r = v, g = p, b = q; break;
-	}
-	return {
-		r: Math.round(r * 255),
-		g: Math.round(g * 255),
-		b: Math.round(b * 255)
-	};
+function hexToRgb(color) {
+	return [
+		parseInt(color.substr(1,2), 16),
+		parseInt(color.substr(3,2), 16),
+		parseInt(color.substr(5,2), 16)
+	];
+}
+function rgbToHex(r, g, b) {
+	return `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
+}
+function rgbToHsl(r, g, b) {
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if(max == min){
+        h = s = 0; // achromatic
+    }else{
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max){
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    return [h, s, l];
+}
+function hslToRgb(h, s, l) {
+    var r, g, b;
+
+    if(s == 0){
+        r = g = b = l; // achromatic
+    }else{
+        var hue2rgb = function hue2rgb(p, q, t){
+            if(t < 0) t += 1;
+            if(t > 1) t -= 1;
+            if(t < 1/6) return p + (q - p) * 6 * t;
+            if(t < 1/2) return q;
+            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+        }
+
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
+    }
+
+    return [
+		Math.round(r * 255),
+		Math.round(g * 255),
+		Math.round(b * 255)
+	];
 }
